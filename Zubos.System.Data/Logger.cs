6bea53e4c703a6ddf7e0bbbb040e211d826bf2e4
emcrank <aaron.todd95@gmail.com>
2016-02-06 13:36:32 +0000

    06022016.13.36

---
+++ Zubos.System.Data/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace Zubos.System.Data
+{
+    public class Logger
+    {
+
+    }
+}
